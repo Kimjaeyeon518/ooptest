38ee0ff930272fb79569ec847443abbb40b0845b
jaeyeon <jae518@naver.com>
2022-02-15 00:44:19 +0900

    Initial commit

---
+++ ooptest/src/main/java/com/biginsight/ooptest/OoptestApplication.java
@@ -0,0 +1,13 @@
+package com.biginsight.ooptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoptestApplication.class, args);
+	}
+
+}
---
+++ ooptest/src/test/java/com/biginsight/ooptest/OoptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biginsight.ooptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
