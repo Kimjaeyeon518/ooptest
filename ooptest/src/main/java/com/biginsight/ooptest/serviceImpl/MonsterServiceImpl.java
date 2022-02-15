e8cd5a0ddef6f2b1e5ad9c11510a87ff1a51991b
jaeyeon <jae518@naver.com>
2022-02-15 16:17:20 +0900

    MonsterService Test 초기 세팅

---
+++ ooptest/src/main/java/com/biginsight/ooptest/serviceImpl/MonsterServiceImpl.java
@@ -0,0 +1,8 @@
+package com.biginsight.ooptest.serviceImpl;
+
+import com.biginsight.ooptest.service.MonsterService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MonsterServiceImpl implements MonsterService {
+}
---
+++ ooptest/src/test/java/com/biginsight/ooptest/serviceImpl/MonsterServiceImplTest.java
@@ -0,0 +1,26 @@
+package com.biginsight.ooptest.serviceImpl;
+
+import com.biginsight.ooptest.repository.MonsterRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class MonsterServiceImplTest {
+
+    @InjectMocks
+    private MonsterServiceImpl monsterService;
+
+    @Mock
+    private MonsterRepository monsterRepository;
+}
